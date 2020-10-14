e6bfa6f21ead46842d5f2d3a919fac5eeea9d9bd
Sangwoo96 <nsw5296@naver.com>
2020-10-14 11:10:10 +0900

    fragment, implicitIntentActicity 추가

---
+++ hw2_3_fragment/app/src/main/java/com/hufs/hw2_3_fragment/FragmentA.java
@@ -0,0 +1,18 @@
+package com.hufs.hw2_3_fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FragmentA extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_a, container, false);
+    }
+}
---
+++ hw2_3_fragment/app/src/main/java/com/hufs/hw2_3_fragment/MainActivity.java
@@ -0,0 +1,32 @@
+package com.hufs.hw2_3_fragment;
+
+        import androidx.appcompat.app.AppCompatActivity;
+        import androidx.fragment.app.Fragment;
+        import androidx.fragment.app.FragmentManager;
+        import androidx.fragment.app.FragmentTransaction;
+
+        import android.os.Bundle;
+        import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View view) {
+        Fragment fr = null;
+        if(view.getId() == R.id.buttonA){
+            fr = new FragmentA();
+        }else if(view.getId() == R.id.buttonB){
+            fr = new FragmentB();
+        }
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction ft = fragmentManager.beginTransaction();
+        ft.replace(R.id.fragment_container,fr);
+        ft.commitAllowingStateLoss();
+    }
+}
